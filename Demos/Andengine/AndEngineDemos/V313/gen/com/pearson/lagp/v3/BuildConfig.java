9f9f78dd8f4ce858668cc6f00db84bf0fbea668d
thaind <ndthaik1@gmail.com>
2013-03-29 18:15:44 +0700

    Andengine

---
+++ Demos/Andengine/AndEngineDemos/V313/gen/com/pearson/lagp/v3/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.pearson.lagp.v3;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Demos/Andengine/AndEngineDemos/VampiresInBackyard/gen/com/pearson/lagp/vinb/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.pearson.lagp.vinb;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
